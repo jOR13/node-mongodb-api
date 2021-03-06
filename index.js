57626aa3aa01b7da754d3f8bf66330465d59e51e
jor1hp <ochoarj13@hotmail.com>
2021-03-06 11:08:07 -0700

    deploy

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+// import routes
+const authRoutes = require("./routes/auth");
+const dashboadRoutes = require('./routes/dashboard');
+const verifyToken = require('./routes/validate-token');
+// cors
+const cors = require('cors');
+
+require("dotenv").config();
+
+const app = express();
+
+var corsOptions = {
+  origin: '*', // Reemplazar con dominio
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+
+// Conexión a Base de datos
+const uri =`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.achiw.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Base de datos conectada"))
+  .catch((e) => console.log("error db:", e));
+
+// import routes
+
+// route middlewares
+app.use("/api/user", authRoutes);
+app.use('/api/dashboard', verifyToken, dashboadRoutes);
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.json({
+    estado: true,
+    mensaje: "funciona!",
+  });
+});
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`servidor andando en: ${PORT}`);
+});

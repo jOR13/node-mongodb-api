2573f296a1ca51251e24b3b5cbe56553227176f6
jor1hp <ochoarj13@hotmail.com>
2021-03-10 14:25:09 -0700

    .

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors(corsOptions));
 
 var corsOptions = {
-  origin: "http://192.168.1.70:3000", // Reemplazar con dominiox
+  origin: "*", // Reemplazar con dominiox
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
